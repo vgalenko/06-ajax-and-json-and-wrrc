fcf5e8d309c6ff5e757d0b0a8a62df0a59edc362
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-08-21 17:03:59 -0700

    local storage and page load working

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -50,7 +50,7 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
-    Article.loadAll(localStorage.rawData); //DONE: What do we pass in to loadAll()?
+    Article.loadAll(JSON.parse(localStorage.rawData)); //DONE: What do we pass in to loadAll()?
     //DONE: What method do we call to render the index page?
     articleView.initIndexPage();
   } else {
@@ -59,7 +59,15 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-    $.getJSON('starter-code/data/hackerIpsum.json').then(function(data){
-        Article.loadAll(data)},
-        function(error){
-        console.log(error)});
+    $.getJSON('/data/hackerIpsum.json')
+    .then(
+      function(data){
+        localStorage.setItem('rawData', JSON.stringify(data));
+        Article.loadAll(data)
+        articleView.initIndexPage();
+      },
+      function(error){
+        console.log(error)
+      });   
+  }
+};
